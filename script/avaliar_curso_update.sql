b3c70e099d84404ba6cd4297582c7da06cc1862c
Vitor Bertelli <102909348+vitorbertelli@users.noreply.github.com>
2023-06-09 12:10:34 -0300

    Add trigger avaliar_curso_update

---
+++ script/avaliar_curso_update.sql
@@ -0,0 +1,10 @@
+DELIMITER $$
+CREATE TRIGGER avaliar_curso_update BEFORE UPDATE
+ON funcionario_curso
+FOR EACH ROW
+BEGIN
+	IF NEW.avaliacao > 5.0 or NEW.avaliacao < 0 THEN
+		SIGNAL SQLSTATE'45000' SET MESSAGE_TEXT = 'A nota para o curso tem que ser entre 0 e 5.';
+	END IF;
+END $$
+DELIMITER ;
